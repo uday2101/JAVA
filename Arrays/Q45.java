a1f5e6dd3b7d16dde8566152f823bb97c733870b
uday2101 <ndc.uday@gmail.com>
2022-05-14 20:46:00 +0530

    New Commit

---
+++ Arrays/Q45.java
@@ -0,0 +1,18 @@
+public class Q45 {
+    static int countRotations(int[] arr, int n){
+        int min = arr[0];
+        int min_index = 0;
+        for(int i = 0; i<n; i++){
+            if(min>arr[i]){
+                min = arr[i];
+                min_index = i;
+            }
+        }
+        return min_index;
+    }
+    public static void main(String[]args){
+        int arr[] = {15,18,2,3,6,12};
+        int n = arr.length;
+        System.out.println(countRotations(arr, n));
+    }
+}
