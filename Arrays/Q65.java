3b01bd5369e0c37876a889fedb47f636b41daa52
uday2101 <ndc.uday@gmail.com>
2022-05-18 19:59:04 +0530

    New Commit

---
+++ Arrays/Q65.java
@@ -0,0 +1,18 @@
+public class Q65 {
+    static void findTriplets(int[] arr, int n, int sum){
+        for(int i = 0; i<n-2; i++){
+            for(int j = i+1; j<n-1; j++){
+                for(int k = j+1; k<n; k++){
+                    if(arr[i]+arr[j]+arr[k] == sum){
+                        System.out.println(arr[i]+", "+arr[j]+", "+arr[k]);
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[]args){
+        int arr[] = {0,-1,2,-3,1};
+        int n = arr.length;
+        findTriplets(arr, n, -2);
+    }
+}
