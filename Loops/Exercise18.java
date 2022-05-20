fb583873f23b35a3cf1387f5540953948f936604
uday2101 <ndc.uday@gmail.com>
2022-05-20 14:27:40 +0530

    New Commit

---
+++ Loops/Exercise18.java
@@ -0,0 +1,13 @@
+public class Exercise18 { //GCD or HCF for two numbers
+    public static void main(String[]args){
+        int n1 = 81;
+        int n2 = 153;
+        int gcd = 1;
+        for(int i = 1; i<=n1 && i<=n2; ++i){
+            if(n1%i==0 && n2%i==0){
+                gcd = i;
+            }
+        }
+        System.out.println("GCD of "+n1+" and "+n2+" is : "+gcd);
+    }
+}
