b6357d4d53ef0be7f7649abe39cb845164542419
uday2101 <ndc.uday@gmail.com>
2022-05-21 16:43:58 +0530

    New Commit

---
+++ Strings/Basic.java
@@ -0,0 +1,11 @@
+public class Basic {
+    public static void main(String[]args){
+        String s = "Welcome";
+        char[] ch = {'s','t','r','i','n','g','s'};
+        String s2 = new String(ch); // converting char array into string 
+        String s3 = new String("example");
+        System.out.println(s);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
