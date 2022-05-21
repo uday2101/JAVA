51024df5912aa33ab29cf102d9a2b9cbed5a68c2
uday2101 <ndc.uday@gmail.com>
2022-05-21 16:47:34 +0530

    New Commit

---
+++ Strings/Q2.java
@@ -0,0 +1,9 @@
+public class Q2 { //compareTo method 
+    public static void main(String[]args){
+        String str1 = "This is Exercise 1";
+        String str2 = "This is Exercise 2";
+        System.out.println("String 1 is : " +str1);
+        System.out.println("String 2 is : " +str2);
+        System.out.println(str1.compareTo(str2));
+    }
+}
