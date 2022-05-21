5d6f15dad480a7146e362202d2c836080b9dcfd1
uday2101 <ndc.uday@gmail.com>
2022-05-21 16:46:43 +0530

    New Commit

---
+++ Strings/Q1.java
@@ -0,0 +1,9 @@
+public class Q1 {
+    public static void main(String[]args){
+        String str = "Java Exercises !";
+        char ch = str.charAt(0);
+        char ch1 = str.charAt(6);
+        System.out.println("The Position at index 0 is : " +ch);
+        System.out.println("The Position at index 6 is : " +ch1);
+    }
+}
