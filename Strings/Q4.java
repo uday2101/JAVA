708f5d4b99910d469958739baf9970c7fa2180e3
uday2101 <ndc.uday@gmail.com>
2022-05-22 18:11:34 +0530

    New Commit

---
+++ Strings/Q4.java
@@ -0,0 +1,7 @@
+public class Q4 { //concatenation of string 
+    public static void main(String[]args){
+        String str1 = "Uday";
+        String str2 = "Agarwal";
+        System.out.println("Concatenated String is : " +str1.concat(" "+str2));
+    }
+}
