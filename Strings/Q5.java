dd931e4ab8fe3e0f049d98f91e47bfbfe3bc7a04
uday2101 <ndc.uday@gmail.com>
2022-05-22 18:12:36 +0530

    New Commit

---
+++ Strings/Q5.java
@@ -0,0 +1,7 @@
+public class Q5 { //demonstration of contains() method
+    public static void main(String[]args){
+        String name = "what do you know about me ";
+        System.out.println(name.contains("what do"));
+        System.out.println(name.contains("about"));
+    }
+}
