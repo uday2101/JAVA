65a59c6e656af915bf14a99ebab685ca7c293258
uday2101 <ndc.uday@gmail.com>
2022-05-23 23:31:07 +0530

    New Commit

---
+++ Strings/Q9.java
@@ -0,0 +1,9 @@
+public class Q9 {
+    public static void main(String[]args){
+        String columnist1 = "Stephen Edwing King";
+        String columnist2 = "Walter Winchell";
+        String columnist3 = "Mike Royko";
+        System.out.println(columnist1.equals(columnist2));
+        System.out.println(columnist2.equals(columnist3));
+    }
+}
