a5c09d17d48b20d7a4b3daa1a1b410d35b1da2f9
uday2101 <ndc.uday@gmail.com>
2022-05-23 23:31:43 +0530

    New Commit

---
+++ Strings/Q10.java
@@ -0,0 +1,7 @@
+public class Q10 {
+    public static void main(String[]args){
+        String str = "Michael Phelps";
+        String str2 = "michael corleone";
+        System.out.println(str.equalsIgnoreCase(str2));
+    }
+}
