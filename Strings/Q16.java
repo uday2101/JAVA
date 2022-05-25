494ec8349a336e13567d0b73cd01ec5afbbcdeaf
uday2101 <ndc.uday@gmail.com>
2022-05-25 10:18:54 +0530

    New Commit

---
+++ Strings/Q16.java
@@ -0,0 +1,7 @@
+public class Q16 {
+    public static void main(String[]args){
+        String name = "example.com";
+        int n = name.length();
+        System.out.println("The length of the given string is : " +n);
+    }
+}
