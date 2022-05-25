6fa38ea0e10fdfa0b1ff9fb29841a7f835bc5fa9
uday2101 <ndc.uday@gmail.com>
2022-05-25 10:17:04 +0530

    New Commit

---
+++ Strings/Q13.java
@@ -0,0 +1,7 @@
+public class Q13 {
+    public static void main(String[]args){
+        String str = "Python Exercises.";
+        int hash_code = str.hashCode();
+        System.out.println(hash_code);
+    }
+}
