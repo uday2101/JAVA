f843cc860616c761a0a04b6d29167873846a429c
uday2101 <ndc.uday@gmail.com>
2022-05-26 21:09:35 +0530

    New Commit

---
+++ Strings/Q21.java
@@ -0,0 +1,8 @@
+public class Q21 {
+    public static void main(String[]args){
+        String str = "The quick brown fox jumps over the lazy dog.";
+        String s1 = str.substring(4, 9);
+        System.out.println(str);
+        System.out.println(s1);
+    }
+}
