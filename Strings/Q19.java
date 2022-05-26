8da674c23181032b4ec710e4753fa88834742eb8
uday2101 <ndc.uday@gmail.com>
2022-05-26 21:08:10 +0530

    New Commit

---
+++ Strings/Q19.java
@@ -0,0 +1,7 @@
+public class Q19 {
+    public static void main(String[]args){
+        String str = "The quick brown fox jumps over the lazy dog";
+        String s2 = str.replace("fox", "cat");
+        System.out.println("The modified string is : " +s2);
+    }
+}
