40bc304959cc026e6ee5a8d85509b4497efc7c77
uday2101 <ndc.uday@gmail.com>
2022-05-26 21:08:56 +0530

    New Commit

---
+++ Strings/Q20.java
@@ -0,0 +1,11 @@
+public class Q20 {
+    public static void main(String[]args){
+        String str1 = "Red is favourite colour";
+        String str2 = "Orange is also my favourite colour";
+        String st = "Red";
+        boolean starts1 = str1.startsWith(st);
+        boolean starts2 = str2.startsWith(st);
+        System.out.println(starts1);
+        System.out.println(starts2);
+    }
+}
