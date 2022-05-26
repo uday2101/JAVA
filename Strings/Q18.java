33da6577123d5444e9c3c14630a50f146933c6e7
uday2101 <ndc.uday@gmail.com>
2022-05-26 21:07:23 +0530

    New Commit

---
+++ Strings/Q18.java
@@ -0,0 +1,7 @@
+public class Q18 {
+    public static void main(String[]args){
+        String str = "The quick brown fox jumps over the lazy dog";
+        String s1 = str.replace('d', 'f');
+        System.out.println(s1);
+    }
+}
