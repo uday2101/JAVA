12e8394022623fab1757f07353ce1f2580d634e3
uday2101 <ndc.uday@gmail.com>
2022-05-27 22:38:41 +0530

    New Commit

---
+++ Strings/Q24.java
@@ -0,0 +1,8 @@
+public class Q24 {
+    public static void main(String[]args){
+        String str = " Java Exercises ";
+        String st = str.trim();
+        System.out.println(str);
+        System.out.println(st);
+    }
+}
